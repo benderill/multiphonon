b5b76877879cd2c053bc74275e7e1042b463e4c6
Anoop Chandran <anoopkcn@gmail.com>
2021-11-20 17:13:01 +0100

    Add a simple setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='multiphonon',
+    version='0.0.1',
+    description='Multiphonon',
+    py_modules=['multiphonon'],
+    package_dir={'': 'src'},
+)
