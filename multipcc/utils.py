c72af6011d08408b14eacea8d902b42ff5be8cb2
Anoop Chandran <anoopkcn@gmail.com>
2021-11-21 01:29:59 +0100

    Add support function return values

--- multipcc/utils.py
+++ multipcc/utils.py
@@ -1,61 +1,52 @@
 import numpy as np
 import scipy.constants as sc
-from scipy import sqrt
+from addict import Dict
 
 '''
 Utility and support functions for multipcc.
 '''
 
 
-def physical_constants(data):
-    phycon = data.root.physical_constants
+def description():
+    '''
+    TODO: This function should be handled in some other way.
+    '''
+    return 'Case 3: Directly calculate the radiative capture cross section by putting the photon density of states as 1/(2pi^2)((hbar omega)^2/(hbar c/eta_R)^3).'
+
+
+def physical_constants():
+    phycon = Dict()
 
     # eV to Joule conversion factor
-    amu_kg, _, _ = sc.physical_constants['atomic mass unit-kilogram relationship']
+    phycon.amu_kg, _, _ = sc.physical_constants['atomic mass unit-kilogram relationship']
     # eV to Joule conversion factor
-    eVJ, _, _ = sc.physical_constants['electron volt-joule relationship']
+    phycon.eVJ, _, _ = sc.physical_constants['electron volt-joule relationship']
     # Bohr Radius in m
-    a_br, _, _ = sc.physical_constants['Bohr radius']
+    phycon.a_br, _, _ = sc.physical_constants['Bohr radius']
     # Rydberg constant in eV
-    r_h, _, _ = sc.physical_constants['Rydberg constant times hc in eV']
+    phycon.r_h, _, _ = sc.physical_constants['Rydberg constant times hc in eV']
     # planck constant over 2pi in Js
-    hbar, _, _ = sc.physical_constants['Planck constant over 2 pi']
+    phycon.hbar, _, _ = sc.physical_constants['Planck constant over 2 pi']
     # planck constant in Js
-    hplanck, _, _ = sc.physical_constants['Planck constant']
+    phycon.hplanck, _, _ = sc.physical_constants['Planck constant']
     # charge of one electron
-    Qe, _, _ = sc.physical_constants['elementary charge']
+    phycon.Qe, _, _ = sc.physical_constants['elementary charge']
     # electron mass in kg
-    m_e, _, _ = sc.physical_constants['electron mass']
+    phycon.m_e, _, _ = sc.physical_constants['electron mass']
     # boltzmann constant in JK^-1
-    kBoltzmann, _, _ = sc.physical_constants['Boltzmann constant']
+    phycon.kB, _, _ = sc.physical_constants['Boltzmann constant']
     # dimensionless
-    alpha, _, _ = sc.physical_constants['fine-structure constant']
+    phycon.alpha, _, _ = sc.physical_constants['fine-structure constant']
     # speed of light in vacuum in m/s
-    c, _, _ = sc.physical_constants['speed of light in vacuum']
-    epsilon_0 = 8.85418782E-12  # in F/m
+    phycon.c, _, _ = sc.physical_constants['speed of light in vacuum']
+    phycon.eps_0 = 8.85418782E-12  # in F/m
 
-    phycon.amu_kg = amu_kg
-    phycon.eVJ = eVJ
-    phycon.a_br = a_br
-    phycon.r_h = r_h
-    phycon.hbar = hbar
-    phycon.hplanck = hplanck
-    phycon.Qe = Qe
-    phycon.m_e = m_e
-    phycon.kB = kBoltzmann
-    phycon.alpha = alpha
-    phycon.c = c
-    phycon.eps_0 = epsilon_0
+    return phycon
 
 
-def description(data):
-    description = data.root.description
-    description.current_simulation = 'Case 3: Directly calculate the radiative capture cross section by putting the photon density of states as 1/(2pi^2)((hbar omega)^2/(hbar c/eta_R)^3).'
-
-
-def input_parameters(data, obj):
-    phycon = data.root.physical_constants
-    inputs = data.root.inputs
+def input_parameters(obj):
+    phycon = physical_constants()
+    inputs = Dict()
 
     inputs.mat = obj.mat  # Material Name (string)
     inputs.T = obj.T  # Temperature in kelvin (float)
@@ -86,11 +77,12 @@ def input_parameters(data, obj):
     inputs.dE = obj.dE  # step size for final energy mesh
     inputs.dir = obj.dir  # multiphonon coupling type
 
+    return inputs
 
-def derived_parameters(data):
-    phycon = data.root.physical_constants
-    inputs = data.root.inputs
-    derived = data.root.derived
+
+def derived_parameters(inputs):
+    phycon = physical_constants()
+    derived = Dict()
 
     derived.a_ebr = phycon.a_br * (inputs.epsilon_l / phycon.eps_0) / (
         inputs.M_eff / phycon.m_e)  # effective Bohr radius in meters
@@ -98,26 +90,66 @@ def derived_parameters(data):
     derived.r_eh = phycon.Qe**2 / \
         (8 * sc.pi * inputs.epsilon_l * derived.a_ebr * phycon.eVJ)
     # refractive index of the material
-    derived.eta_r = sqrt(inputs.epsilon_h / phycon.eps_0)
+    derived.eta_r = np.sqrt(inputs.epsilon_h / phycon.eps_0)
     derived.sa = 4 * \
-        sqrt(sc.pi * derived.r_eh *
-             phycon.eVJ / (phycon.kB * inputs.T))
+        np.sqrt(sc.pi * derived.r_eh * phycon.eVJ / (phycon.kB * inputs.T))
+
+    return derived
 
 
-def bias_voltage(data):
-    inputs = data.root.inputs
-    VB = data.root.bias_voltage
+def bias_voltage(start, stop, step):
+    bias_volt = Dict()
+    bias_volt.Vb = np.arange(start, stop + step, step)
+    bias_volt.NVb = len(bias_volt.Vb)
+    return bias_volt
 
-    VB.Vb = np.arange(inputs.V_start, inputs.V_stop + inputs.dV, inputs.dV)
-    VB.NVb = len(VB.Vb)
+
+def energy_grids(inputs, derived):
+    phycon = physical_constants()
+    egrid = Dict()
+
+    # Distance of the defect from the band(the energy grids are so chosen to get the right nu)
+    egrid.ET = np.linspace(4*derived.r_eh, inputs.Eg - 4*derived.r_eh, 1000)
+    # Distance of the defect from the nearest band
+    egrid.deltaE = np.minimum(egrid.ET, inputs.Eg - egrid.ET)
+    egrid.nu = np.sqrt(derived.r_eh / egrid.deltaE)  # nu
+    # the maximum final energy is 3KbT away
+    egrid.Ekmax = 3 * phycon.kB * inputs.T / phycon.eVJ
+    # the final energy mesh.
+    egrid.Ek = np.arange(inputs.dE, egrid.Ekmax, inputs.dE)
+    # the corresponding k values
+    egrid.k = np.sqrt(egrid.Ek * phycon.eVJ * 2 *
+                      inputs.M_eff / phycon.hbar**2)
+    return egrid
+
+
+def matrices(inputs, egrid):
+    mat = Dict()
+
+    mat.mat2D = np.zeros([egrid.ET.size, egrid.Ek.size])  # xy
+
+    # 2D matrix of trap energy [ET x Ek]
+    mat.ET2D = np.broadcast_to(egrid.ET, (egrid.Ek.size, egrid.ET.size)).T
+    # 2D matrix of final energy state [ET x Ek]
+    mat.Ek2D = np.broadcast_to(egrid.Ek, (egrid.ET.size, egrid.Ek.size))
+    # 3D matrix of final energy state [3 x ET x Ek]
+    mat.Ek3D = np.broadcast_to(egrid.Ek, (3, egrid.ET.size, egrid.Ek.size))
+
+    mat.Ec = egrid.Ek + inputs.Eg
+    mat.Ev = -egrid.Ek
+
+    # 2D matrix of nu [ET x Ek]
+    mat.nu2D = np.broadcast_to(egrid.nu, (egrid.Ek.size, egrid.ET.size)).T
+    mat.nu3D = np.tile(mat.nu2D, (3, 1)).reshape(
+        3, egrid.ET.size, egrid.Ek.size)  # 3D matrix of nu [3 x ET x Ek]
+    return mat
 
 
 def coulomb_factor(data):
-    phycon = data.root.physical_constants
     derived = data.root.derived
     egrid = data.root.energy_grids
-    cf = data.root.coulomb_factor
     mat = data.root.matrix
+    cf = data.root.coulomb_factor
 
     cf.CF_pos = np.zeros(mat.mat2D.shape)
     cf.CF_neg = np.zeros(mat.mat2D.shape)
@@ -145,8 +177,8 @@ def w_function(data, arg, axis):
     mat = data.root.matrix
     wf = data.root.weighing_function
 
-    wf.Gc = (8 * sqrt(2) * sc.pi / phycon.hplanck**3) * \
-        sqrt(inputs.M_eff)**3 * \
+    wf.Gc = (8 * np.sqrt(2) * sc.pi / phycon.hplanck**3) * \
+        np.sqrt(inputs.M_eff)**3 * \
         np.sqrt((mat.Ec - inputs.Eg) * phycon.eVJ)  # DOS
     wf.fE = np.exp((inputs.Eg/2 - mat.Ec) * phycon.eVJ /
                    (phycon.kB * inputs.T))  # fermidirac fucntion
@@ -168,8 +200,8 @@ def w_function_2(data, arg, axis):
     mat = data.root.matrix
     wf2 = data.root.weighing_function
 
-    wf2.Gc = (8 * sqrt(2) * sc.pi / phycon.hplanck**3) * \
-        sqrt(inputs.M_eff)**3 * \
+    wf2.Gc = (8 * np.sqrt(2) * sc.pi / phycon.hplanck**3) * \
+        np.sqrt(inputs.M_eff)**3 * \
         np.sqrt((mat.Ec - inputs.Eg) * phycon.eVJ)  # DOS
     wf2.fE = np.exp((inputs.Eg/2 - mat.Ec) * phycon.eVJ /
                     (phycon.kB * inputs.T))  # fermidirac fucntion
@@ -184,50 +216,3 @@ def w_function_2(data, arg, axis):
     wf2.weighted_func = wf2.weighted_numerator / \
         wf2.denominator  # weighted function
     return(wf2.weighted_func)
-
-
-def energy_grids(data):
-    phycon = data.root.physical_constants
-    inputs = data.root.inputs
-    derived = data.root.derived
-    egrid = data.root.energy_grids
-
-    # Distance of the defect from the band(the energy grids are so chosen to get the right nu)
-    egrid.ET = np.linspace(4*derived.r_eh, inputs.Eg - 4*derived.r_eh, 1000)
-    # Distance of the defect from the nearest band
-    egrid.deltaE = np.minimum(egrid.ET, inputs.Eg - egrid.ET)
-    egrid.nu = np.sqrt(derived.r_eh / egrid.deltaE)  # nu
-    # the maximum final energy is 3KbT away
-    egrid.Ekmax = 3 * phycon.kB * inputs.T / phycon.eVJ
-    # the final energy mesh.
-    egrid.Ek = np.arange(inputs.dE, egrid.Ekmax, inputs.dE)
-    # the corresponding k values
-    egrid.k = sqrt(egrid.Ek * phycon.eVJ * 2 *
-                   inputs.M_eff / phycon.hbar**2)
-
-
-def matrices(data):
-    phycon = data.root.physical_constants
-    inputs = data.root.inputs
-    derived = data.root.derived
-    egrid = data.root.energy_grids
-    dos = data.root.effective_density_of_states
-    VB = data.root.bias_voltage
-    mat = data.root.matrix
-
-    mat.mat2D = np.zeros([egrid.ET.size, egrid.Ek.size])  # xy
-
-    # 2D matrix of trap energy [ET x Ek]
-    mat.ET2D = np.broadcast_to(egrid.ET, (egrid.Ek.size, egrid.ET.size)).T
-    # 2D matrix of final energy state [ET x Ek]
-    mat.Ek2D = np.broadcast_to(egrid.Ek, (egrid.ET.size, egrid.Ek.size))
-    # 3D matrix of final energy state [3 x ET x Ek]
-    mat.Ek3D = np.broadcast_to(egrid.Ek, (3, egrid.ET.size, egrid.Ek.size))
-
-    mat.Ec = egrid.Ek + inputs.Eg
-    mat.Ev = -egrid.Ek
-
-    # 2D matrix of nu [ET x Ek]
-    mat.nu2D = np.broadcast_to(egrid.nu, (egrid.Ek.size, egrid.ET.size)).T
-    mat.nu3D = np.tile(mat.nu2D, (3, 1)).reshape(
-        3, egrid.ET.size, egrid.Ek.size)  # 3D matrix of nu [3 x ET x Ek]
